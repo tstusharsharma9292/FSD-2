8cf8165d8ed0dcd24f848382a77f34fdfb88b354
Tushar Sharma <tstusharsharma9292@gmail.com>
2026-07-22 11:08:28 +0530

    day 1

---
+++ DAY1.js
@@ -0,0 +1,45 @@
+console.log("hellow world");
+
+// Variables
+
+var name = "viaks";
+
+console.log(name);
+
+console.log(typeof name);
+
+var name = "tushar"; // redecleration
+console.log(name);
+
+//let
+
+let agee = 22;
+
+console.log(agee);
+
+//datatypes
+
+var name ='vikas';
+console.log(typeof(name));
+
+var age = 22;
+console.log(typeof(agee));
+
+let s={
+    name:"amit"
+
+
+
+}
+
+console.log(s);
+
+var a=5;
+var b=3;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a++);
+console.log(a--);
