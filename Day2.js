abf6a121f0ee1a9cf0236382dedfa8ee926613c4
Tushar Sharma <tstusharsharma9292@gmail.com>
2026-09-07 09:49:25 +0530

    practice

--- Day2.js
+++ Day2.js
@@ -1,76 +1,97 @@
-// /* Three types of loops in JavaScript are:
-// 1. For Loop: Used to execute a block of code a specific number of times.
-// 2. While Loop: Used to execute a block of code as long as a specified condition is true.
-// 3. Do...While Loop: Similar to the while loop, but it executes the block of code at least once before checking the condition. */
+// // /* Three types of loops in JavaScript are:
+// // 1. For Loop: Used to execute a block of code a specific number of times.
+// // 2. While Loop: Used to execute a block of code as long as a specified condition is true.
+// // 3. Do...While Loop: Similar to the while loop, but it executes the block of code at least once before checking the condition. */
 
-// // Indefinite For loop
+// // // Indefinite For loop
 
-// // for (;;) {
-// //     console.log("running....");
+// // // for (;;) {
+// // //     console.log("running....");
+// // // }
+
+
+// // /* Practice Statements 
+// //    - Sum of digits
+// //    - Reverse of a number 
+// //    - Fibonacci 
+// //    - Prime */
+
+// // // 1- Sum of digits
+
+// // let num = 12345
+// // let sum = 0
+
+// // while(num != 0 ){
+// //     let digit = num % 10;
+// //     sum += digit
+// //     num = Math.floor(num/10)    
 // // }
 
-
-// /* Practice Statements 
-//    - Sum of digits
-//    - Reverse of a number 
-//    - Fibonacci 
-//    - Prime */
-
-// // 1- Sum of digits
-
-// let num = 12345
-// let sum = 0
-
-// while(num != 0 ){
-//     let digit = num % 10;
-//     sum += digit
-//     num = Math.floor(num/10)    
-// }
-
-// console.log(`The sum of the digits is: ${sum}`)
+// // console.log(`The sum of the digits is: ${sum}`)
 
 
-// // Reverse of a number
+// // // Reverse of a number
 
-// let num2 = 124578
-// let revNum = 0
+// // let num2 = 124578
+// // let revNum = 0
 
-// while(num2 != 0){
-//     const digit = num2 % 10
-//     revNum = (revNum*10) + digit
-//     num2 = Math.floor(num2/10)
-// }
+// // while(num2 != 0){
+// //     const digit = num2 % 10
+// //     revNum = (revNum*10) + digit
+// //     num2 = Math.floor(num2/10)
+// // }
 
-// console.log(`Reversed Number is: ${revNum}`);
+// // console.log(`Reversed Number is: ${revNum}`);
 
 
 
 
-// //string
-// let x=10;
-// Str1=string('10');
-// typeof(Str1);
+// // //string
+// // let x=10;
+// // Str1=string('10');
+// // typeof(Str1);
 
-let Str="My name is tushar sharma";
-console.log(Str);
-let Str1="S"+Str.slice(1);
-console.log(Str1);
-console.log(Str);
+// let Str="My name is tushar sharma";
+// console.log(Str);
+// let Str1="S"+Str.slice(1);
+// console.log(Str1);
+// console.log(Str);
 
-const str2="Hello";
+// const str2="Hello";
 
-const str3=new String('Hello');
+// const str3=new String('Hello');
 
-console.log(str2==str3);
-console.log(str2===str3);
-const fruits=["apple","banana","grapes"];
-const copy=fruits.slice();
-console.log(fruits);
-console.log(copy);
-console.log(copy.slice(-1));
-console.log(copy.slice(-2));
+// console.log(str2==str3);
+// console.log(str2===str3);
+// const fruits=["apple","banana","grapes"];
+// const copy=fruits.slice();
+// console.log(fruits);
+// console.log(copy);
+// console.log(copy.slice(-1));
+// console.log(copy.slice(-2));
 
-const arr=[1,2,3,4,5];
-const removed=arr.splice(1,2);
-console.log(removed);
-console.log(arr);
+// const arr=[1,2,3,4,5];
+// const removed=arr.splice(1,2);
+// console.log(removed);
+// console.log(arr);
+
+
+function show() {
+    console.log(this == window);
+}
+
+show();
+window.show();
+
+const person = {
+    name: "Utkarsh",
+    age: 18,
+    greet: function() {
+        return this.name;
+    }
+};
+
+let p1 = person.greet;
+
+console.log(person.greet()); 
+
